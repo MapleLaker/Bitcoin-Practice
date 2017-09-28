8313d0e433044798e4ebd549f142361ad7760b14
MapleLaker <31602441+MapleLaker@users.noreply.github.com>
2017-09-28 08:20:51 +0800

    Set up Project
    Practice Bitcoin in C#

---
+++ NBitcoin-master/ProgrammingBlockchain/Chapters/Chapter1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace ProgrammingBlockchain.Chapters
+{
+	class Chapter1
+	{
+
+		public void Lesson1()
+		{
+			/*
+            Key key = new Key(); //generates a new private key.     
+            PubKey pubKey = key.PubKey; //gets the matching public key.     
+            Console.WriteLine("Public Key: {0}", pubKey);
+            KeyId hash = pubKey.Hash; //gets a hash of the public key.     
+            Console.WriteLine("Hashed public key: {0}", hash);
+            BitcoinAddress address = pubKey.GetAddress(Network.Main); //retrieves the bitcoin address.     
+            Console.WriteLine("Address: {0}", address);
+            Script scriptPubKeyFromAddress = address.ScriptPubKey;
+            Console.WriteLine("ScriptPubKey from address: {0}", scriptPubKeyFromAddress);
+            Script scriptPubKeyFromHash = hash.ScriptPubKey;
+            Console.WriteLine("ScriptPubKey from hash: {0}", scriptPubKeyFromHash);
+            */
+
+			Key privatekey = new Key();
+			PubKey publicKey = privatekey.PubKey;
+			Console.WriteLine(publicKey);
+
+			KeyId hash = publicKey.Hash;
+			Console.WriteLine(hash);
+
+			BitcoinAddress mainaddress = publicKey.GetAddress(Network.Main);
+			Console.WriteLine(mainaddress);
+
+		}
+
+
+	}
+}
+
+
---
+++ NBitcoin-master/ProgrammingBlockchain/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgrammingBlockchain.Chapters;
+
+namespace ProgrammingBlockchain
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var chapter1 = new Chapter1();
+			chapter1.Lesson1();
+			Console.WriteLine("\n Press Enter to Continue");
+			Console.ReadLine();
+		}
+	}
+}
